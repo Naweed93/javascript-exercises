0e3bb5ba842b85da7bba3640065e9d0ea7298ad0
navid ashraf <ashraf.navid71@gmail.com>
2022-03-08 01:13:41 +0330

    Complete 05_sumAll Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
 
+    if(typeof(first)!=='number' || typeof(second)!=='number' || first<0) return "ERROR";
+    if(second<first){
+        let swap = first;
+        first = second;
+        second = swap;
+    }
+    for (i = first; i<=second;i++){
+        sum+= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
