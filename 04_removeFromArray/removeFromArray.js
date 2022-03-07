7e28fde0d1fedd2a58b03d1a317c03f29a0b8829
navid ashraf <ashraf.navid71@gmail.com>
2022-03-08 01:01:41 +0330

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for(i of arguments){
+        while(true){
+            let index = array.indexOf(i)
+            if(index === -1) break;
+            array.splice(index, 1)
+        }
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
